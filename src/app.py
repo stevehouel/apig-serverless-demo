52bc6711721e8be02bb407d59564221265556fa1
Steve Houël <steve.houel.perso@gmail.com>
2020-02-27 17:31:44 +0100

    Fix code

--- src/app.py
+++ src/app.py
@@ -1,17 +1,12 @@
 import os, boto3, json
 
-ESCALATION_INTENT_MESSAGE="Seems that you are having troubles with our service. Would you like to be transferred to the associate?"
-FULFILMENT_CLOSURE_MESSAGE="Seems that you are having troubles with our service. Let me transfer you to the associate."
-
-escalation_intent_name = 'Escalate'
 client = boto3.client('comprehend')
 
 def lambda_handler(event, context):
     # Get body content
     body = event.get('body')
-    body = json.loads(body)
 
-    sentiment=client.detect_sentiment(Text=body.get('message'),LanguageCode='fr')
+    sentiment=client.detect_sentiment(Text=body,LanguageCode='fr')
     return {
        'statusCode': status,
        'body': json.dumps(sentiment, indent=2),
